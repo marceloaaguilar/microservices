afce1a3e500f8979ec2db842ae42e81b54bdbb0b
Marcelo D'almeida <marcelodalmeida42@gmail.com>
2024-10-23 23:40:52 -0300

    init

---
+++ task-service/src/main/java/com/example/task_service/Task.java
@@ -0,0 +1,29 @@
+package com.example.task_service;
+
+public class Task {
+    private int id;
+    private String description;
+
+    public Task(){}
+
+    public Task (int id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setName(String description) {
+        this.description = description;
+    }
+}
---
+++ task-service/src/main/java/com/example/task_service/TaskApplication.java
@@ -0,0 +1,15 @@
+package com.example.task_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class TaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskApplication.class, args);
+    }
+}
+
